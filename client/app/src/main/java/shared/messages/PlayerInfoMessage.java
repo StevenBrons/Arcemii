ddecd33ab868e3c02071928ee4ff5e9798b6ba42
Borroot <borroot@live.nl>
2019-05-25 10:35:27 +0200

    Heartbeat mechanism for connection lost implemented.

--- client/app/src/main/java/shared/messages/PlayerInfoMessage.java
+++ client/app/src/main/java/shared/messages/PlayerInfoMessage.java
@@ -2,6 +2,8 @@ package shared.messages;
 
 /**
  * This class can be used to send a new name for a player to the server.
+ * Or to request a name by the server during connection initialization.
+ * @author Bram Pulles
  */
 public class PlayerInfoMessage extends Message {
 
