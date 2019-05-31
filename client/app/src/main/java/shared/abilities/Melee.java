37fb4086b9a89bfc5d353e63bbb7d1265d43f392
Borroot <borroot@live.nl>
2019-05-31 16:27:20 +0200

    Ability classes added.

---
+++ client/app/src/main/java/shared/abilities/Melee.java
@@ -0,0 +1,13 @@
+package shared.abilities;
+
+import shared.entities.Entity;
+import shared.general.Level;
+
+public class Melee extends Ability {
+
+	@Override
+	public boolean execute(Level level, Entity self) {
+		// TODO: Implement melee ability.
+		return false;
+	}
+}
---
+++ client/app/src/main/java/shared/abilities/Range.java
@@ -0,0 +1,13 @@
+package shared.abilities;
+
+import shared.entities.Entity;
+import shared.general.Level;
+
+public class Range extends Ability {
+
+	@Override
+	public boolean execute(Level level, Entity self) {
+		// TODO: Implement the range ability.
+		return false;
+	}
+}
