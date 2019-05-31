053e2354dafbc2205a521f28a1144f4733161cf0
Borroot <borroot@live.nl>
2019-05-31 16:27:20 +0200

    Ability classes added.

---
+++ client/app/src/main/java/shared/abilities/Heal.java
@@ -0,0 +1,13 @@
+package shared.abilities;
+
+import shared.entities.Entity;
+import shared.general.Level;
+
+public class Heal extends Ability {
+
+	@Override
+	public boolean execute(Level level, Entity self) {
+		// TODO: Implement heal ability.
+		return false;
+	}
+}
