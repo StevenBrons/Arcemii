33e486608f378d53ecb0b9704d12a90e363b9e98
Thomas Berghuis <thomberg99@gmail.com>
2019-05-31 16:22:55 +0200

    Buttons added to the frame layout

--- client/app/src/main/java/client/activities/GameActivity.java
+++ client/app/src/main/java/client/activities/GameActivity.java
@@ -3,14 +3,19 @@ package client.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import com.debernardi.archemii.R;
 
 import client.controller.ClientGameHandler;
 import client.view.GameView;
+import client.view.JoystickView;
 import client.view.Texture;
 import shared.general.Level;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements JoystickView.JoystickListener {
 
     private GameView view;
 
@@ -27,6 +32,9 @@ public class GameActivity extends AppCompatActivity {
         Texture.init(getAssets());
         FrameLayout frame = new FrameLayout(this);
         frame.addView(view);
+        LayoutInflater factory = LayoutInflater.from(this);
+        View UI = factory.inflate(R.layout.ui, null);
+        frame.addView(UI);
         setContentView(frame);
         ClientGameHandler.handler.setGameActivity(this);
     }
@@ -36,5 +44,22 @@ public class GameActivity extends AppCompatActivity {
             view.updateLevel(level);
             view.postInvalidate();
         }
+    }
+
+    public void onAbilityBottom(View view){
+
+    }
+
+    public void onAbilityMiddle(View view){
+
+    }
+
+    public void onAbilityUpper(View view){
+
+    }
+
+    @Override
+    public void onJoystickMoved(float xPercent, float yPercent, int source) {
+
     }
 }
