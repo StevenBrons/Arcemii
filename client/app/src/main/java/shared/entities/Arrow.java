ab57bd89e20f8d8edef53bb83bfa625e01e8c884
Jelmer <spaanse.aak@gmail.com>
2019-06-04 12:19:41 +0200

    Fix arrow direction

--- client/app/src/main/java/shared/entities/Arrow.java
+++ client/app/src/main/java/shared/entities/Arrow.java
@@ -36,7 +36,7 @@ public class Arrow extends Entity{
 		List<RenderItem> result = new ArrayList<>();
 		RenderItem renderItem = new RenderItem("arrowHorizontal",
 				(int)(Tile.WIDTH*xPos),-(int)(Tile.HEIGHT*yPos),0.5,0.5,2);
-		renderItem.setRotation(180.0f/(float)Math.PI*(float)Math.atan2((float)yVel,(float) xVel));
+		renderItem.setRotation(180.0f/(float)Math.PI*(float)Math.atan2((float)-yVel,(float) xVel));
 		result.add(renderItem);
 		return result;
 	}
