3704d7d584e3d87dac8297bfe8b668ac160610cc
Borroot <borroot@live.nl>
2019-06-06 21:03:40 +0200

    Changed ip.

--- client/app/src/main/java/client/controller/Connection.java
+++ client/app/src/main/java/client/controller/Connection.java
@@ -22,7 +22,7 @@ public class Connection {
 	public static final String TAG = "CONNECTION";
 
 	// Jelmers server ip: 213.124.165.68
-	// For emulators ip: 10.0.0.2
+	// For emulators ip: 10.0.2.2
 	private String hostName = "213.124.165.68";
 	private final int PORT = 26194;
 	private boolean isConnected = false;
