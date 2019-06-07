26f0bd0a382965995969e9f74b806a217594bd60
Borroot <borroot@live.nl>
2019-06-07 11:11:11 +0200

    Everyone unready after reset.

--- client/app/src/main/java/client/activities/GameActivity.java
+++ client/app/src/main/java/client/activities/GameActivity.java
@@ -25,208 +25,199 @@ import shared.general.Level;
 
 public class GameActivity extends AppCompatActivity implements JoystickView.JoystickListener, MediaPlayer.OnCompletionListener {
 
-    private GameView view;
-    private static MediaPlayer audio;
-    private static boolean muted;
+	private GameView view;
+	private static MediaPlayer audio;
+	private static boolean muted;
 
-    /**
-     * Makes a new GameView view in the activity and starts the refresh loop
-     * @param savedInstanceState
-     * @author Jelmer Firet
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        muted = getSharedPreferences("audioprefs", MODE_PRIVATE).contains("muted");
-        view = new GameView(this);
-        Texture.init(getAssets());
-        FrameLayout frame = new FrameLayout(this);
-        frame.addView(view);
-        LayoutInflater factory = LayoutInflater.from(this);
-        View UI = factory.inflate(R.layout.ui, null);
-        Player me = ClientGameHandler.handler.getPlayer();
-        ArrayList<Ability> myAbilities;
-        synchronized (me){
-            myAbilities = me.getAbilities();
-        }
-        frame.addView(UI);
-        setContentView(frame);
+	/**
+	 * Makes a new GameView view in the activity and starts the refresh loop
+	 * @param savedInstanceState
+	 * @author Jelmer Firet
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		muted = getSharedPreferences("audioprefs", MODE_PRIVATE).contains("muted");
+		view = new GameView(this);
+		Texture.init(getAssets());
+		FrameLayout frame = new FrameLayout(this);
+		frame.addView(view);
+		LayoutInflater factory = LayoutInflater.from(this);
+		View UI = factory.inflate(R.layout.ui, null);
+		Player me = ClientGameHandler.handler.getPlayer();
+		ArrayList<Ability> myAbilities;
+		synchronized (me){
+			myAbilities = me.getAbilities();
+		}
+		frame.addView(UI);
+		setContentView(frame);
 
-        String uri = "@drawable/";
+		String uri = "@drawable/";
 
-        final ImageButton ability1 = findViewById(R.id.AbilityButtonBottom);
-        final ImageButton ability2 = findViewById(R.id.AbilityButtonMiddle);
-        final ImageButton ability3 = findViewById(R.id.AbilityButtonUpper);
+		final ImageButton ability1 = findViewById(R.id.AbilityButtonBottom);
+		final ImageButton ability2 = findViewById(R.id.AbilityButtonMiddle);
+		final ImageButton ability3 = findViewById(R.id.AbilityButtonUpper);
 
-        String temp = uri + myAbilities.get(1).getId();
-        int imageResource = getResources().getIdentifier(temp, null, getPackageName());
-        Drawable res = getResources().getDrawable(imageResource);
-        ability1.setImageDrawable(res);
+		String temp = uri + myAbilities.get(1).getId();
+		int imageResource = getResources().getIdentifier(temp, null, getPackageName());
+		Drawable res = getResources().getDrawable(imageResource);
+		ability1.setImageDrawable(res);
 
-        temp = uri + myAbilities.get(2).getId();
-        imageResource = getResources().getIdentifier(temp, null, getPackageName());
-        res = getResources().getDrawable(imageResource);
-        ability2.setImageDrawable(res);
+		temp = uri + myAbilities.get(2).getId();
+		imageResource = getResources().getIdentifier(temp, null, getPackageName());
+		res = getResources().getDrawable(imageResource);
+		ability2.setImageDrawable(res);
 
-        temp = uri + myAbilities.get(3).getId();
-        imageResource = getResources().getIdentifier(temp, null, getPackageName());
-        res = getResources().getDrawable(imageResource);
-        ability3.setImageDrawable(res);
-
-
-
-        ClientGameHandler.handler.setGameActivity(this);
-    }
-
-    public void draw(Level level) {
-        if (view != null) {
-            view.updateLevel(level);
-            view.postInvalidate();
-        }
-    }
-
-    /**
-     * Handles clicks on this ability button, fading the button until it is available
-     * @author Robert Koprinkov
-     * */
-    public void onAbilityBottom(View view){
-        Player player = ClientGameHandler.handler.getPlayer();
-        synchronized (player){
-            ((ImageButton)findViewById(R.id.AbilityButtonBottom)).setAlpha((float) 0.6);
-            player.invokeBottom();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(player.getAbilities().get(1).getTimeout());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    findViewById(R.id.AbilityButtonBottom).setAlpha(1);
-                }
-            }).start();
-        }
-    }
-
-    /**
-     * Handles clicks on this ability button, fading the button until it is available
-     * @author Robert Koprinkov
-     * */
-    public void onAbilityMiddle(View view){
-        Player player = ClientGameHandler.handler.getPlayer();
-
-        synchronized (player){
-            ((ImageButton)findViewById(R.id.AbilityButtonMiddle)).setAlpha((float) 0.6);
-            player.invokeMiddle();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(player.getAbilities().get(2).getTimeout());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    findViewById(R.id.AbilityButtonMiddle).setAlpha(1);
-                }
-            }).start();
-        }
-    }
-
-    /**
-     * Handles clicks on this ability button, fading the button until it is available
-     * @author Robert Koprinkov
-     * */
-    public void onAbilityUpper(View view){
-        Player player = ClientGameHandler.handler.getPlayer();
-        synchronized (player){
-            ((ImageButton)findViewById(R.id.AbilityButtonUpper)).setAlpha((float) 0.6);
-            player.invokeUpper();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(player.getAbilities().get(3).getTimeout());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    findViewById(R.id.AbilityButtonUpper).setAlpha(1);
-                }
-            }).start();
-        }
-    }
+		temp = uri + myAbilities.get(3).getId();
+		imageResource = getResources().getIdentifier(temp, null, getPackageName());
+		res = getResources().getDrawable(imageResource);
+		ability3.setImageDrawable(res);
 
 
-    public void updateHealth(int number){
-        TextView tv1 = findViewById(R.id.textView3);
-        String w = "" + number;
-        tv1.setText(w);
-    }
 
-    @Override
-    public void onJoystickMoved(float xPercent, float yPercent, int source) {
-        Player player = ClientGameHandler.handler.getPlayer();
-        double range = 0.2;
-        double angle = -Math.atan2(yPercent,xPercent);
-        if (Math.pow(xPercent,2) + Math.pow(yPercent,2) > Math.pow(range,2)) {
-            synchronized (player){
-                player.direction = angle;
-                player.doMove = true;
-            }
-        } else {
-            synchronized (player){
-                player.doMove = false;
-            }
-        }
+		ClientGameHandler.handler.setGameActivity(this);
+	}
 
-    }
+	public void draw(Level level) {
+		if (view != null) {
+			view.updateLevel(level);
+			view.postInvalidate();
+		}
+	}
 
-    /**
-    * handler to load and start music on start of activity
-    * @author Thijs van Loenhout
-    */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        audio = MediaPlayer.create(this,R.raw.game);
-        if(!muted)
-            audio.start();
-        audio.setLooping(true);
-    }
+	/**
+	 * Handles clicks on this ability button, fading the button until it is available
+	 * @author Robert Koprinkov
+	 * */
+	public void onAbilityBottom(View view){
+		Player player = ClientGameHandler.handler.getPlayer();
+		synchronized (player){
+			findViewById(R.id.AbilityButtonBottom).setAlpha((float) 0.6);
+			player.invokeBottom();
+			new Thread(() -> {
+				try {
+					Thread.sleep(player.getAbilities().get(1).getTimeout());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				findViewById(R.id.AbilityButtonBottom).setAlpha(1);
+			}).start();
+		}
+	}
 
-    /**
-     * handler to pause music when activity is paused
-     * @author Thijs van Loenhout
-     */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(audio.isPlaying())
-            audio.pause();
-    }
+	/**
+	 * Handles clicks on this ability button, fading the button until it is available
+	 * @author Robert Koprinkov
+	 * */
+	public void onAbilityMiddle(View view){
+		Player player = ClientGameHandler.handler.getPlayer();
 
-    /**
-     *
-     *
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(!muted)
-                audio.start();
-    }
+		synchronized (player){
+			findViewById(R.id.AbilityButtonMiddle).setAlpha((float) 0.6);
+			player.invokeMiddle();
+			new Thread(() -> {
+				try {
+					Thread.sleep(player.getAbilities().get(2).getTimeout());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				findViewById(R.id.AbilityButtonMiddle).setAlpha(1);
+			}).start();
+		}
+	}
 
-    /**
-     * handler to stop and release music when activity stops
-     * @author Thijs van Loenhout
-     */
-    @Override
-    protected void onStop() {
-        super.onStop();
-        audio.release();
-    }
+	/**
+	 * Handles clicks on this ability button, fading the button until it is available
+	 * @author Robert Koprinkov
+	 * */
+	public void onAbilityUpper(View view){
+		Player player = ClientGameHandler.handler.getPlayer();
+		synchronized (player){
+			findViewById(R.id.AbilityButtonUpper).setAlpha((float) 0.6);
+			player.invokeUpper();
+			new Thread(() -> {
+				try {
+					Thread.sleep(player.getAbilities().get(3).getTimeout());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				findViewById(R.id.AbilityButtonUpper).setAlpha(1);
+			}).start();
+		}
+	}
 
-    @Override
-    public void onCompletion(MediaPlayer mp) {
-        audio.start();
-    }
+
+	public void updateHealth(int number){
+		TextView tv1 = findViewById(R.id.textView3);
+		String w = "" + number;
+		tv1.setText(w);
+	}
+
+	@Override
+	public void onJoystickMoved(float xPercent, float yPercent, int source) {
+		Player player = ClientGameHandler.handler.getPlayer();
+		double range = 0.2;
+		double angle = -Math.atan2(yPercent,xPercent);
+		if (Math.pow(xPercent,2) + Math.pow(yPercent,2) > Math.pow(range,2)) {
+			synchronized (player){
+				player.direction = angle;
+				player.doMove = true;
+			}
+		} else {
+			synchronized (player){
+				player.doMove = false;
+			}
+		}
+
+	}
+
+	/**
+	 * handler to load and start music on start of activity
+	 * @author Thijs van Loenhout
+	 */
+	@Override
+	protected void onStart() {
+		super.onStart();
+		audio = MediaPlayer.create(this,R.raw.game);
+		if(!muted)
+			audio.start();
+		audio.setLooping(true);
+	}
+
+	/**
+	 * handler to pause music when activity is paused
+	 * @author Thijs van Loenhout
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if(audio.isPlaying())
+			audio.pause();
+	}
+
+	/**
+	 *
+	 *
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if(!muted)
+			audio.start();
+	}
+
+	/**
+	 * handler to stop and release music when activity stops
+	 * @author Thijs van Loenhout
+	 */
+	@Override
+	protected void onStop() {
+		super.onStop();
+		audio.release();
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		audio.start();
+	}
 }
